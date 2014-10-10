e952a662d36b8146a5abf3dc85e55c78dcf5135c
Slvr <slvrgauthier@gmail.com>
2014-10-10 15:37:04 +0200

    339

--- Fac/Master/IN339/Compresseur.cpp
+++ Fac/Master/IN339/Compresseur.cpp
@@ -4,13 +4,39 @@
 
 using namespace std;
 
+Image toDiff(Image* image) {
+	Image imageOut(image);
+	if(image->getFormat() == PGM) {
+		int w = image->getWidth(), h = image->getHeight();
+		for(unsigned int j=1 ; j < h ; j++) {
+			int xy = j * w;
+			imageOut.setData(xy, image->getData(xy) - image->getData(xy - w) + 128);
+		}
+		for(unsigned int i=1 ; i < w ; i++) {
+			int xy = i;
+			imageOut.setData(xy, image->getData(xy) - image->getData(xy - 1) + 128);
+		}
+		for(unsigned int j=1 ; j < h ; j++) {
+			for(unsigned int i=1 ; i < w ; i++) {
+				int xy = j * w + i;
+				imageOut.setData(xy, image->getData(xy) - (image->getData(xy - 1) + image->getData(xy - w)) / 2 + 128);
+			}
+		}
+	}
+	else {
+		cout << "Format incompatible avec la différence." << endl;
+	}
+	
+	return imageOut;
+}
+
 int main(int argc, char **argv) {
 	string name, name2;
 	cout << "Nom de l'image : ";
 	cin >> name;
 	name = "img/" + name;
 	Image imIn(name);
-	Image imOut(imIn);
+	//Image imOut(imIn);
 	
 	int w = imIn.getWidth(), h = imIn.getHeight();
 	int action = -1;
@@ -24,7 +50,9 @@ int main(int argc, char **argv) {
 		cout << "2 : Calculer le PSNR" << endl;
 		cout << "3 : Compresser l'image en RGBc" << endl;
 		cout << "4 : Compresser l'image en YCCc" << endl;
-		cout << "5 : Convertir l'image en PPM" << endl;
+		cout << "5 : Convertir l'image en PGM" << endl;
+		cout << "6 : Convertir l'image en PPM" << endl;
+		cout << "7 : Faire une différence" << endl;
 		cout << "===== " << name << " =====" << endl;
 
 		cout << endl << "Action : ";
@@ -36,7 +64,7 @@ int main(int argc, char **argv) {
 				cin >> name;
 				name = "img/" + name;
 				imIn.load(name);
-				imOut.load(name);
+				//imOut.load(name);
 				break;
 			case 2 : 
 				cout << "Nom de l'image à comparer : ";
@@ -52,7 +80,9 @@ int main(int argc, char **argv) {
 				break;
 			case 3 : imIn.save(name, RGBC); break;
 			case 4 : imIn.save(name, YCCC); break;
-			case 5 : imIn.save(name, PPM); break;
+			case 5 : imIn.save(name, PGM); break;
+			case 6 : imIn.save(name, PPM); break;
+			case 7 : toDiff(&imIn).save(name, PGM); break;
 			default : cout << "Action inconnue" << endl; break;
 		}
 	}
