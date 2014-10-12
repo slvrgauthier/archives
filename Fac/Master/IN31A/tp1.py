ecf5e7b38c5c1058dd07197f52ca106791ab954d
Slvr <slvrgauthier@gmail.com>
2014-10-12 11:06:31 +0200

    31A

--- Fac/Master/IN31A/tp1.py
+++ Fac/Master/IN31A/tp1.py
@@ -3,53 +3,50 @@ import math
 
 class Kdnode:
 	def __init__(self):
-		location,left_child,right_child,parent = None,None,None,None
+		location,left_child,right_child = None,None,None
 
-def createkdtree(point_list, depth, minbucketsize, parent = None):
+def createkdtree(point_list, depth, minbucketsize):
 	if len(point_list) <= minbucketsize or depth == 0:
-		return [parent, point_list]
+		return point_list
 	axis = depth % 3
 	point_list.sort(key=lambda point: point[axis])
 	median = len(point_list)/2
 	node = Kdnode()
 	node.location = point_list[median]
-	node.left_child = createkdtree(point_list[:median], depth-1, minbucketsize, node)
-	node.right_child = createkdtree(point_list[median+1:], depth-1, minbucketsize, node)
-	node.parent = parent
+	node.left_child = createkdtree(point_list[:median], depth-1, minbucketsize)
+	node.right_child = createkdtree(point_list[median+1:], depth-1, minbucketsize)
 	return node
 
-def closestpoint(kdtree, p, depth = 0, candidat = None, down = True):
+def closestpoint(kdtree, p, depth):
 	axis = depth % 3
 	if isinstance(kdtree, Kdnode):
-		""" Branche """
-		if down == True:
-			""" Parcours descendant """
-			if p[axis] > kdtree.location[axis]:
-				return closestpoint(kdtree.right_child, p, depth+1, candidat, True)
-			else:
-				return closestpoint(kdtree.left_child, p, depth+1, candidat, True)
+		""" Move down the tree recursively """
+		if p[axis] > kdtree.location[axis]:
+			candidat = closestpoint(kdtree.right_child, p, depth-1)
 		else:
-			""" Parcours ascendant """
-			if candidat == None or norm(p-kdtree.location) < norm(p-candidat):
-				candidat = kdtree.location
-			if kdtree.parent == None or norm(p-candidat) < abs(p[axis]-kdtree.location[axis]):
+			candidat = closestpoint(kdtree.left_child, p, depth-1)
+			
+		""" Unwind the recursion of the tree """
+		if candidat == None or norm(p-kdtree.location) < norm(p-candidat):
+			candidat = kdtree.location
+		if norm(p-candidat) < abs(p[axis]-kdtree.location[axis]):
+			return candidat
+		else:
+			if p[axis] > kdtree.location[axis]:
+				other_candidat = closestpoint(kdtree.left_child, p, depth-1)
+			else:
+				other_candidat = closestpoint(kdtree.right_child, p, depth-1)
+			if other_candidat == None or norm(p-candidat) < norm(p-other_candidat):
 				return candidat
 			else:
-				if kdtree.parent.left_child == kdtree:
-					other_candidat = closestpoint(kdtree.parent.right_child, p, depth-1)
-				else:
-					other_candidat = closestpoint(kdtree.parent.left_child, p, depth-1)
-				if norm(p-candidat) < norm(p-other_candidat):
-					return candidat
-				else:
-					return other_candidat
+				return other_candidat
 	else:
-		""" Feuille """
+		""" Leaf node """
 		candidat = None
-		for c in kdtree[1]:
+		for c in kdtree:
 			if candidat == None or norm(p-c) < norm(p-candidat):
 				candidat = c
-		return closestpoint(kdtree[0], p, depth-1, candidat, False)
+		return candidat
 
 def brute_force_closest(point, pointlist):
 	""" Find the closest points of 'point' in 'pointlist' using a brute force approach """
@@ -74,11 +71,9 @@ def test_kdtree(create_kdtree_func, closestpoint_func, nbtest = 100, nbpoints = 
 	mkdtree = create_kdtree_func(points, depth, minbucketsize)
 	for i in xrange(nbtest):
 		testpoint = generate_random_point()
-		kpid = closestpoint_func(mkdtree, testpoint)
+		kpid = closestpoint_func(mkdtree, testpoint, depth)
 		bfpid = brute_force_closest(testpoint, points)
-		print kpid, bfpid
 		if kpid != bfpid: 
-			"raise ValueError('Invalid closest point')"
-			print 'Invalid closest point'
+			raise ValueError('Invalid closest point')
 
-test_kdtree(createkdtree, closestpoint, 1, 10, 3, 1)
+test_kdtree(createkdtree, closestpoint)
