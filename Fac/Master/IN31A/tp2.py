7a34ef6f206e11525c25b90df1f3457f85eb98ef
Slvr <slvrgauthier@gmail.com>
2014-10-17 19:47:06 +0200

    31A

--- Fac/Master/IN31A/tp2.py
+++ Fac/Master/IN31A/tp2.py
@@ -44,7 +44,7 @@ class GMap:
 		""" Associate coordinates with the vertex &lt;alpha_1,alpha_2&gt;(dart) """
 		self.positions[dart] = position
 
-	def orbit(self, dart,list_of_alpha_value):
+	def orbit(self, dart, list_of_alpha_value):
 		""" Return the orbit of dart using a list of alpha relation."""
 		""" Example of use. gmap.orbit(0,[0,1])."""
 		""" In python, you can use the set structure to process only once all darts of the orbit."""
@@ -75,14 +75,39 @@ class GMap:
 		""" Determine first the orbits of dart to sew."""
 		"""   Check if they are compatible"""
 		"""   Sew pairs of corresponding darts """
+		list_of_alpha_value = []
+		for i in range(0,degree):
+			list_of_alpha_value.append(i)
+		orbit1 = self.orbit(dart1, list_of_alpha_value)
+		orbit2 = self.orbit(dart2, list_of_alpha_value)
+		if len(orbit1) == len(orbit2):
+			for d1 in orbit1:
+				for d2 in orbit2:
+					if d1 in self.positions and d2 in self.positions and self.positions[d1] == self.positions[d2]:
+						if degree == 0:
+							self.link_dart(degree, d1, d2)
+						elif self.positions[self.alpha_0[d1]] == self.positions[self.alpha_0[d2]]:
+							if degree == 1:
+								self.link_dart(degree, d1, d2)
+							elif self.positions[self.alpha_1[d1]] == self.positions[self.alpha_1[d2]]:
+								if degree == 2:
+									self.link_dart(degree, d1, d2)
+
+	def nb_elements(self, degree):
+		if degree == 0:
+			return len(self.alpha_0)
+		if degree == 1:
+			return len(self.alpha_1)
+		if degree == 2:
+			return len(self.alpha_2)
 
 def cube(xsize,ysize,zsize):
 	gmap = GMap()
 	
 	# front face
-	for i in range(0,16):
+	for i in range(0,48):
 		gmap.add_dart(i)
-	for i in range(0,8):
+	for i in range(0,24):
 		gmap.link_dart(0,2*i,2*i+1)
 		if (2*i+2)%8 == 0:
 			gmap.link_dart(1,2*i+1,2*i-6)
@@ -106,20 +131,94 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(13, [+xsize, -ysize, +zsize])
 	gmap.set_position(14, [+xsize, -ysize, +zsize])
 	gmap.set_position(15, [-xsize, -ysize, +zsize])
-		
-	return gmap
 	
-def nb_elements(degree): pass
-def eulercharacteristic (gmap): pass
-def display(gmap): pass
+	# left face
+	gmap.set_position(16, [-xsize, -ysize, +zsize])
+	gmap.set_position(17, [-xsize, +ysize, +zsize])
+	gmap.set_position(18, [-xsize, +ysize, +zsize])
+	gmap.set_position(19, [-xsize, +ysize, -zsize])
+	gmap.set_position(20, [-xsize, +ysize, -zsize])
+	gmap.set_position(21, [-xsize, -ysize, -zsize])
+	gmap.set_position(22, [-xsize, -ysize, -zsize])
+	gmap.set_position(23, [-xsize, -ysize, +zsize])
+	
+	# right face
+	gmap.set_position(24, [+xsize, -ysize, -zsize])
+	gmap.set_position(25, [+xsize, +ysize, -zsize])
+	gmap.set_position(26, [+xsize, +ysize, -zsize])
+	gmap.set_position(27, [+xsize, +ysize, +zsize])
+	gmap.set_position(28, [+xsize, +ysize, +zsize])
+	gmap.set_position(29, [+xsize, -ysize, +zsize])
+	gmap.set_position(30, [+xsize, -ysize, +zsize])
+	gmap.set_position(31, [+xsize, -ysize, -zsize])
+	
+	# down face
+	gmap.set_position(32, [-xsize, -ysize, +zsize])
+	gmap.set_position(33, [-xsize, -ysize, -zsize])
+	gmap.set_position(34, [-xsize, -ysize, -zsize])
+	gmap.set_position(35, [+xsize, -ysize, -zsize])
+	gmap.set_position(36, [+xsize, -ysize, -zsize])
+	gmap.set_position(37, [+xsize, -ysize, +zsize])
+	gmap.set_position(38, [+xsize, -ysize, +zsize])
+	gmap.set_position(39, [-xsize, -ysize, +zsize])
+	
+	# up face
+	gmap.set_position(40, [-xsize, +ysize, -zsize])
+	gmap.set_position(41, [-xsize, +ysize, +zsize])
+	gmap.set_position(42, [-xsize, +ysize, +zsize])
+	gmap.set_position(43, [+xsize, +ysize, +zsize])
+	gmap.set_position(44, [+xsize, +ysize, +zsize])
+	gmap.set_position(45, [+xsize, +ysize, -zsize])
+	gmap.set_position(46, [+xsize, +ysize, -zsize])
+	gmap.set_position(47, [-xsize, +ysize, -zsize])
+	
+	#sew
+	gmap.sew_dart(2, 0, 16)
+	gmap.sew_dart(2, 0, 24)
+	gmap.sew_dart(2, 0, 32)
+	gmap.sew_dart(2, 0, 40)
+	
+	gmap.sew_dart(2, 8, 16)
+	gmap.sew_dart(2, 8, 24)
+	gmap.sew_dart(2, 8, 32)
+	gmap.sew_dart(2, 8, 40)
+	
+	gmap.sew_dart(2, 16, 32)
+	gmap.sew_dart(2, 16, 40)
+	
+	gmap.sew_dart(2, 24, 32)
+	gmap.sew_dart(2, 24, 40)
+	
+	return gmap
+
+def eulercharacteristic (gmap):
+	#FIXME calculer les 6 et 4... positions individuelles ?
+	S = len(gmap.points) / 6
+	A = gmap.nb_elements(0) / 4
+	F = 6
+	return S - A + F
+
+def display(gmap):
+	display_face(gmap.darts(), range(0,8), [255,0,0])
+	display_face(gmap.darts(), range(8,16), [255,255,0])
+	display_face(gmap.darts(), range(16,24), [0,255,0])
+	display_face(gmap.darts(), range(24,32), [0,255,255])
+	display_face(gmap.darts(), range(32,40), [0,0,255])
+	display_face(gmap.darts(), range(40,48), [255,0,255])
 
 def display_face(pointlist, indexlist, color):
 	sc = Scene([Shape(FaceSet(pointlist,indexlist),Material(color))])
 	Viewer.display(sc)
 
 gmap = cube(10, 10, 10)
-print "darts() :", gmap.darts()
-#print "alpha_0 :", gmap.alpha_0
-#print "alpha_1 :", gmap.alpha_1
+print "darts() :", len(gmap.darts()), "points"
+print "alpha_0 :", gmap.nb_elements(0), "points"
+print "alpha_1 :", gmap.nb_elements(1), "points"
+print "alpha_2 :", gmap.nb_elements(2), "points"
 print "is_valid() :", gmap.is_valid()
-print "orbit(0, [0,1]) :", gmap.orbit(0, [0,1])
+print "orbit(0, [0,1]) :", len(gmap.orbit(0, [0,1])), "points"
+print "orbit(0, [1,2]) :", len(gmap.orbit(0, [1,2])), "points"
+print "orbit(0, [0,2]) :", len(gmap.orbit(0, [0,2])), "points"
+print "orbit(0, [0,1,2]) :", len(gmap.orbit(0, [0,1,2])), "points"
+print "euler() :", eulercharacteristic(gmap)
+#display(gmap)
