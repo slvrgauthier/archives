38aa6a6ac9846a55b6e0139c1bd9334d944af966
Slvr <slvrgauthier@gmail.com>
2014-10-18 20:33:05 +0200

    31A

--- Fac/Master/IN31A/tp2.py
+++ Fac/Master/IN31A/tp2.py
@@ -3,7 +3,7 @@ from sets import Set
 
 class GMap:
 	def __init__(self):
-		self.points = []
+		self.brins = []
 		self.alpha_0 = {}
 		self.alpha_1 = {}
 		self.alpha_2 = {}
@@ -11,17 +11,17 @@ class GMap:
 
 	def darts(self):
 		""" Return a list of id representing the darts of the structure """
-		return self.points
+		return self.brins
 
 	def add_dart(self, element):
 		""" Create a new dart and return its id """
-		self.points.append(element)
-		return self.points.index(element)
+		self.brins.append(element)
+		return self.brins.index(element)
 
 	def is_valid(self):
 		""" Test the validity of the structure."""
 		""" Check if there is pending dart for alpha_0 and alpha_1 (fixed point) """
-		for element in self.points :
+		for element in self.brins :
 			if element in self.alpha_0 and self.alpha_0[self.alpha_0[element]] != element:
 				return False
 			if element in self.alpha_1 and self.alpha_1[self.alpha_1[element]] != element:
@@ -104,7 +104,6 @@ class GMap:
 def cube(xsize,ysize,zsize):
 	gmap = GMap()
 	
-	# front face
 	for i in range(0,48):
 		gmap.add_dart(i)
 	for i in range(0,24):
@@ -113,6 +112,8 @@ def cube(xsize,ysize,zsize):
 			gmap.link_dart(1,2*i+1,2*i-6)
 		else:
 			gmap.link_dart(1,2*i+1,2*i+2)
+	
+	# down
 	gmap.set_position(0, [-xsize, -ysize, -zsize])
 	gmap.set_position(1, [-xsize, +ysize, -zsize])
 	gmap.set_position(2, [-xsize, +ysize, -zsize])
@@ -122,7 +123,7 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(6, [+xsize, -ysize, -zsize])
 	gmap.set_position(7, [-xsize, -ysize, -zsize])
 	
-	# back face
+	# up
 	gmap.set_position(8, [-xsize, -ysize, +zsize])
 	gmap.set_position(9, [-xsize, +ysize, +zsize])
 	gmap.set_position(10, [-xsize, +ysize, +zsize])
@@ -132,7 +133,7 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(14, [+xsize, -ysize, +zsize])
 	gmap.set_position(15, [-xsize, -ysize, +zsize])
 	
-	# left face
+	# back
 	gmap.set_position(16, [-xsize, -ysize, +zsize])
 	gmap.set_position(17, [-xsize, +ysize, +zsize])
 	gmap.set_position(18, [-xsize, +ysize, +zsize])
@@ -142,7 +143,7 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(22, [-xsize, -ysize, -zsize])
 	gmap.set_position(23, [-xsize, -ysize, +zsize])
 	
-	# right face
+	# front
 	gmap.set_position(24, [+xsize, -ysize, -zsize])
 	gmap.set_position(25, [+xsize, +ysize, -zsize])
 	gmap.set_position(26, [+xsize, +ysize, -zsize])
@@ -152,7 +153,7 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(30, [+xsize, -ysize, +zsize])
 	gmap.set_position(31, [+xsize, -ysize, -zsize])
 	
-	# down face
+	# left
 	gmap.set_position(32, [-xsize, -ysize, +zsize])
 	gmap.set_position(33, [-xsize, -ysize, -zsize])
 	gmap.set_position(34, [-xsize, -ysize, -zsize])
@@ -162,7 +163,7 @@ def cube(xsize,ysize,zsize):
 	gmap.set_position(38, [+xsize, -ysize, +zsize])
 	gmap.set_position(39, [-xsize, -ysize, +zsize])
 	
-	# up face
+	# right
 	gmap.set_position(40, [-xsize, +ysize, -zsize])
 	gmap.set_position(41, [-xsize, +ysize, +zsize])
 	gmap.set_position(42, [-xsize, +ysize, +zsize])
@@ -193,32 +194,39 @@ def cube(xsize,ysize,zsize):
 
 def eulercharacteristic (gmap):
 	#FIXME calculer les 6 et 4... positions individuelles ?
-	S = len(gmap.points) / 6
+	S = len(gmap.brins) / 6
 	A = gmap.nb_elements(0) / 4
 	F = 6
 	return S - A + F
 
 def display(gmap):
-	display_face(gmap.darts(), range(0,8), [255,0,0])
-	display_face(gmap.darts(), range(8,16), [255,255,0])
-	display_face(gmap.darts(), range(16,24), [0,255,0])
-	display_face(gmap.darts(), range(24,32), [0,255,255])
-	display_face(gmap.darts(), range(32,40), [0,0,255])
-	display_face(gmap.darts(), range(40,48), [255,0,255])
+	l1 = []
+	l2 = []
+	for i in range(0,24):
+		l1.append(gmap.positions[2*i])
+	for i in range(0,6):
+		l2.append([4*i,4*i+1,4*i+2,4*i+3])
+	display_face(l1,l2,[0,0,0])
+	#display_face([gmap.positions[0],gmap.positions[2],gmap.positions[4],gmap.positions[6]], [[0,1,2,3]], [255,0,0])
+	#display_face([gmap.positions[8],gmap.positions[10],gmap.positions[12],gmap.positions[14]], [[0,1,2,3]], [255,255,0])
+	#display_face([gmap.positions[16],gmap.positions[18],gmap.positions[20],gmap.positions[22]], [[0,1,2,3]], [0,255,0])
+	#display_face([gmap.positions[24],gmap.positions[26],gmap.positions[28],gmap.positions[30]], [[0,1,2,3]], [0,255,255])
+	#display_face([gmap.positions[32],gmap.positions[34],gmap.positions[36],gmap.positions[38]], [[0,1,2,3]], [0,0,255])
+	#display_face([gmap.positions[40],gmap.positions[42],gmap.positions[44],gmap.positions[46]], [[0,1,2,3]], [255,0,255])
 
 def display_face(pointlist, indexlist, color):
 	sc = Scene([Shape(FaceSet(pointlist,indexlist),Material(color))])
 	Viewer.display(sc)
 
-gmap = cube(10, 10, 10)
-print "darts() :", len(gmap.darts()), "points"
-print "alpha_0 :", gmap.nb_elements(0), "points"
-print "alpha_1 :", gmap.nb_elements(1), "points"
-print "alpha_2 :", gmap.nb_elements(2), "points"
+gmap = cube(5, 5, 5)
+print "darts() :", len(gmap.darts()), "brins"
+print "alpha_0 :", gmap.nb_elements(0), "brins"
+print "alpha_1 :", gmap.nb_elements(1), "brins"
+print "alpha_2 :", gmap.nb_elements(2), "brins"
 print "is_valid() :", gmap.is_valid()
-print "orbit(0, [0,1]) :", len(gmap.orbit(0, [0,1])), "points"
-print "orbit(0, [1,2]) :", len(gmap.orbit(0, [1,2])), "points"
-print "orbit(0, [0,2]) :", len(gmap.orbit(0, [0,2])), "points"
-print "orbit(0, [0,1,2]) :", len(gmap.orbit(0, [0,1,2])), "points"
+print "orbit(0, [0,1]) :", len(gmap.orbit(0, [0,1])), "brins"
+print "orbit(0, [1,2]) :", len(gmap.orbit(0, [1,2])), "brins"
+print "orbit(0, [0,2]) :", len(gmap.orbit(0, [0,2])), "brins"
+print "orbit(0, [0,1,2]) :", len(gmap.orbit(0, [0,1,2])), "brins"
 print "euler() :", eulercharacteristic(gmap)
-#display(gmap)
+display(gmap)
